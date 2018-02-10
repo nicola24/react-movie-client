3d4f7bfc911fb22531ea4e65189ccb97c340d6a7
Hack Reactor Student <student@hackreactor.com>
2018-02-10 08:43:53 -0800

    search bar added

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import moviesData from '../data/sampledata';
 import MovieList from './movielist.jsx';
+import SearchBar from '.searchbar.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div>
-          <p>MovieList</p>
+          <h1>MovieList</h1>
+        </div>
+        <div>
+          <SearchBar />
         </div>
         <div>
           <MovieList movielist={moviesData}/>
